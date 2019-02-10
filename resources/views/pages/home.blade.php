c1ea2e259cf68966be583ce77d681814c1540ac5
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2019-02-10 12:54:51 +0700

    add template

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.base')
+
+@section('title')
+Home
+@endsection
+
+<!--  -->
+
+@section('css')
+
+@endsection
+
+<!--  -->
+
+@section('content')
+
+@endsection
+
+<!--  -->
+
+@section('js')
+
+@endsection
